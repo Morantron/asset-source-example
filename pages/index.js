85bbd75904d625e0b777e04974e75324fc339d77
Jorge Morante <jorge@morante.eu>
2022-04-11 18:35:42 +0200

    require.context example

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import yolo from './test.yolo'
+import queries from '../lib/queries'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <pre>{yolo}</pre>
+      <pre>{queries.yoloA}</pre>
     </div>
   )
 }
